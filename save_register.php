53c1d1f5b950d1265548b360b6ddb3f0f4f73312
tmsyfon <86591651+tmsyfon@users.noreply.github.com>
2023-04-14 00:40:46 +0700

    save_register

---
+++ save_register.php
@@ -0,0 +1,52 @@
+<?php
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+?>
+
+<?php include('connect.php');
+
+//สร้างตัวแปร
+$id = $_POST['id'];
+$Username = $_POST['Username'];
+$Password = $_POST['Password'];
+$Firstname = $_POST['Firstname'];
+$Lastname = $_POST['Lastname'];
+$Phone = $_POST['Phone'];
+$House = $_POST['House'];
+$Province = $_POST['Province'];
+$District = $_POST['District'];
+$Subdistrict = $_POST['Subdistrict'];
+$Postalcode = $_POST['Postalcode'];
+
+//เพิ่มข้อมูล
+$sql = " INSERT INTO regis_user
+	(id, Username, Password, Firstname, Lastname, Phone, House, Province, District, Subdistrict, Postalcode)
+	VALUES
+	('$id', '$Username', '$Password', '$Firstname', '$Lastname', '$Phone', '$House', '$Province', '$District', '$Subdistrict', '$Postalcode')";
+
+$result = mysqli_query($con, $sql) or die("Error in query: $sql " . mysqli_error($con));
+
+
+//ปิดการเชื่อมต่อ database
+mysqli_close($con);
+
+
+
+
+//ถ้าสำเร็จให้ขึ้นอะไร
+if ($result) {
+    echo "<script type='text/javascript'>";
+    echo "alert('Register Success');";
+    echo "window.location = 'home1.html';";
+    echo "</script>";
+} else {
+    //กำหนดเงื่อนไขว่าถ้าไม่สำเร็จให้ขึ้นข้อความและกลับไปหน้าเพิ่ม
+    echo "<script type='text/javascript'>";
+    echo "alert('error!');";
+    echo "window.location = 'indexhtml; ";
+    echo "</script>";
+}
+
+
+?>
